c390270ef354721d704f1f49256d38f2659862fc
Sotatek-VietHoang <viet.hoang@sotatek.com>
2024-10-28 12:47:50 +0700

    update base code ads

--- src/AdComponent.js
+++ src/AdComponent.js
@@ -1,21 +1,34 @@
 import React, { useEffect, useRef } from "react";
 
-const AdComponent = ({slot}) => {
-    const adRef = useRef(null);
+const AdComponent = ({ slot }) => {
+  const adRef = useRef(null);
 
-    useEffect(() => {
-      if (adRef.current && !adRef.current.hasAd) {
-        try {
-          (window.adsbygoogle = window.adsbygoogle || []).push({});
-          adRef.current.hasAd = true; // Đánh dấu quảng cáo đã được tải          
-        } catch (e) {
-          console.error("AdSense error:", e);
-        }
+  useEffect(() => {
+    // Tải mã quảng cáo chỉ một lần
+    const loadAdScript = () => {
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5706143351800743";
+      script.crossOrigin = "anonymous";
+      document.body.appendChild(script);
+    };
+
+    if (!window.adsbygoogle) {
+      loadAdScript();
+    }
+
+    if (adRef.current && !adRef.current.hasAd) {
+      try {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        adRef.current.hasAd = true; // Đánh dấu quảng cáo đã được tải          
+      } catch (e) {
+        console.error("AdSense error:", e);
       }
-    }, []);
+    }
+  }, []);
 
   return (
-    <div className="ad-container" style={{ textAlign: "center", margin: "20px 0"}}>
+    <div className="ad-container" style={{ textAlign: "center", margin: "20px 0" }}>
       <ins
         ref={adRef}
         className="adsbygoogle"
@@ -25,7 +38,6 @@ const AdComponent = ({slot}) => {
         data-ad-format="auto"
         data-full-width-responsive="true"
         data-ad-test="on" // Bật chế độ Test Ads
-        // data-ad-status={'filled'}
       ></ins>
     </div>
   );
