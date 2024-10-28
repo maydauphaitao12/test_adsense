c06497fc348815e2fb44b14138bf489d65347e3a
Sotatek-VietHoang <viet.hoang@sotatek.com>
2024-10-28 12:57:22 +0700

    update ads txt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+// src/App.js
+
 import React from "react";
 import "./App.css";
 import AdComponent from "./AdComponent";
 
 function App() {
+  const adsTxtUrl = "https://test-adsense.vercel.app/ads.txt"; // Địa chỉ của ads.txt
+
+  const openAdsTxt = () => {
+    window.open(adsTxtUrl, "_blank"); // Mở liên kết trong tab mới
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My Test AdSense Page</h1>
         <p>Check out the ad below:</p>
-        <AdComponent  slot={'2135685733'} />
+        <AdComponent slot={'2135685733'} />
       </header>
 
       <main className="App-content">
@@ -20,8 +28,8 @@ function App() {
         </section>
 
         {/* Quảng cáo AdSense */}
-        <h2>Quang cao o day</h2>
-        <AdComponent  slot={'6171133665'} />
+        <h2>Quảng cáo ở đây</h2>
+        <AdComponent slot={'6171133665'} />
 
         <section className="content">
           <h2>Another Section</h2>
@@ -29,6 +37,11 @@ function App() {
             This is another section of content. More text and other information can go here.
           </p>
         </section>
+
+        {/* Nút để mở trang ads.txt */}
+        <button onClick={openAdsTxt} style={{ marginTop: '20px' }}>
+          Xem Nội Dung ads.txt
+        </button>
       </main>
 
       <footer className="App-footer">
